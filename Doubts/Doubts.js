856996e63538b783c29659b51f4d77d6731c2a7f
shubham2o <shubhamsharma.ha@gmail.com>
2022-05-26 19:57:09 +0530

    JavaScript Array.from() added

--- Doubts/Doubts.js
+++ Doubts/Doubts.js
@@ -1,4 +1,14 @@
 // JavaScript HTML DOM NodeList Object
 // EXAMPLE 1 ||
 const myNodeList = document.querySelectorAll('p');
-document.getElementById('demo').innerHTML = "The innerHTML of the seconds paragraph is : " + myNodeList[1].innerHTML;
+
+document.getElementById('demo').innerHTML =
+    "The innerHTML of the seconds paragraph is : " + myNodeList[1].innerHTML;
+
+
+// JavaScript Array.from()
+// EXAMPLE 1 ||
+let text = "ABCDEFG";
+const myArr = Array.from(text);
+
+document.getElementById('demo2').innerHTML = myArr;
