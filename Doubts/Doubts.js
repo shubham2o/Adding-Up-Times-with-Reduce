d9e431be597030389a514478f8b35a398adbdd7f
shubham2o <shubhamsharma.ha@gmail.com>
2022-05-26 19:26:49 +0530

    HTML DOM NodeList Object+HTMLCollection & NodeList

---
+++ Doubts/Doubts.js
@@ -0,0 +1,4 @@
+// JavaScript HTML DOM NodeList Object
+// EXAMPLE 1 ||
+const myNodeList = document.querySelectorAll('p');
+document.getElementById('demo').innerHTML = "The innerHTML of the seconds paragraph is : " + myNodeList[1].innerHTML;
