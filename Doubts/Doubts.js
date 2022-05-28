85949b008d1fc5fa2383e42740f3ad85b866beb9
shubham2o <shubhamsharma.ha@gmail.com>
2022-05-28 09:06:22 +0530

    JavaScript parseFloat() added and Doubts completed

--- Doubts/Doubts.js
+++ Doubts/Doubts.js
@@ -12,3 +12,13 @@ let text = "ABCDEFG";
 const myArr = Array.from(text);
 
 document.getElementById('demo2').innerHTML = myArr;
+
+
+// JavaScript parseFloat()
+// EXAMPLE 1 ||
+document.getElementById('demo3').innerHTML =
+    parseFloat(10) + "<br>" +
+    parseFloat("10") + "<br>" +
+    parseFloat("10.33") + "<br>" +
+    parseFloat("34 45 66") + "<br>" +
+    parseFloat("He was 40");
